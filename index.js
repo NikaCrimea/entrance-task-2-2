f7e849869fd25f2686823edc28ac0990da81ac22
NikaCrimea <veronika74069@yandex.ru>
2018-09-13 02:16:26 +0300

    Доработано

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
-        //Скрыть PopUp при загрузке страницы
-        PopUpHide();
-    });
-    //Функция отображения PopUp
-    function PopUpShow(){
-        $("#popup1").show();
-        $('#bg_layer').animate({
-        opacity: 0.5
-    });
-  };
-    //Функция скрытия PopUp
-    function PopUpHide(){
-        $("#popup1").hide();
-    }
+function modal(id) {
+	var modal = document.getElementById(id);
+	var close = document.querySelectorAll('[data-close="true"]');
+	modal.style.display = 'block';
+	for (var i = 0; i < close.length; i++) {
+		close[i].onclick = function() {
+			modal.style.display = 'none';
+		}
+	}
+	window.onclick = function(e){
+		if (e.target == modal) {
+			modal.style.display = 'none';
+		}
+	};
+	document.onkeydown = function(e) {
+		if (e.keyCode == 27) {
+			modal.style.display = 'none';
+		}
+	};
+}
